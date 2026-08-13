fc5b50646785c6c11862b1b5677c9faa0bf12c69
kamalendu Das <kamalendudas901@gmail.com>
2026-08-13 20:56:57 +0530

    Add projects file

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ const PORT=3000
 const path=require("path")
 const methodOverride=require("method-override")
 const ejsMate=require("ejs-mate")
-
 // const cookieParser=require("cookie-parser")
 const session = require("express-session")
+const MongoStore=require("connect-mongo").default
 const flash=require("connect-flash")
 const passport=require("passport")
 const localstrategy=require("passport-local")
@@ -18,6 +18,23 @@ const user=require("./models/user.js")
 const listingRouter = require("./routes/listing.js")
 const reviewRouter = require("./routes/review.js")
 const userRouter=require("./routes/user.js")
+
+
+const dburl=process.env.ATLASDB_URL
+
+// mongoose.connect('mongodb://127.0.0.1:27017/YatriSathi')
+main()
+.then(() => {
+    console.log("MongoDB connected");
+})
+.catch((err) => {
+    console.log(err);
+});
+
+async function main() {
+    await mongoose.connect(dburl)
+}
+
 
 // app.use(cookieParser("secret ode"))
 app.set("view engine","ejs")
@@ -27,8 +44,21 @@ app.use(methodOverride("_method"))
 app.engine('ejs',ejsMate)
 app.use(express.static(path.join(__dirname,"/public")))//static file use r jono
 
+
+const store=MongoStore.create({
+    mongoUrl:dburl,
+    crypto:{
+      secret:process.env.SECRET,
+    },
+    touchAfter:24*3600
+})
+
+store.on("error",(err)=>{
+    console.log("Error in Mongo Session Store",err)
+})
 const sessionOptions={
-    secret:"mysupersecretcode",
+    store,
+    secret:process.env.SECRET,
     resave:false,
     saveUninitialized:true,
     cookie:{
@@ -38,9 +68,7 @@ const sessionOptions={
     }
 }
 
-app.get('/',(req,res)=>{ 
-    res.send("hii i am root")
-})
+
 
 app.use(session(sessionOptions))
 app.use(flash())
@@ -89,13 +117,6 @@ app.use((req,res,next)=>{
    
 // })
 
-mongoose.connect('mongodb://127.0.0.1:27017/YatriSathi')
-.then(() => {
-    console.log("MongoDB connected");
-})
-.catch((err) => {
-    console.log(err);
-});
 
 
 
